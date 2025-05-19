760ff6b16c2de72c1d91b1dbc34b210bb5d250d9
shravyapendota <shravyapendota76@gmail.com>
2025-05-19 10:28:40 +0530

    Initial commit: added DSA files

--- class.py
+++ class.py
@@ -1,4 +1,14 @@
-class Test:
-    id=1212
-t=Test()
-print(t.id)
+class Emp:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+    def dsply(self):
+        print(self.name)
+        print(self.id)
+
+
+e1=Emp("shrav", 101)
+e1.dsply()
+e2=Emp("ks", 102)
+e2.dsply()
